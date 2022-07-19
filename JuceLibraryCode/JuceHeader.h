9f19fff2bd6c14da652a3802e541e41f4f067f4f
Arthur Poiret <contact@arthurpoiret.fr>
2022-07-19 23:02:13 +0200

    chore: up version to 0.0.5

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -40,7 +40,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "owlplug-scanner";
     const char* const  companyName    = "";
-    const char* const  versionString  = "0.0.4";
-    const int          versionNumber  = 0x4;
+    const char* const  versionString  = "0.0.5";
+    const int          versionNumber  = 0x5;
 }
 #endif
